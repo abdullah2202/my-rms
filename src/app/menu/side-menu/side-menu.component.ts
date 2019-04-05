6ff190985899403b18baeabf11b9bccd07a3e7ee
Mohammad Abdullah <mabdul2202@gmail.com>
2019-04-05 22:32:22 +0100

    Accordian Menu Style

--- src/app/menu/side-menu/side-menu.component.ts
+++ src/app/menu/side-menu/side-menu.component.ts
@@ -8,16 +8,44 @@ import * as $ from 'jquery';
 })
 export class SideMenuComponent implements OnInit {
 
+  activatedMenuItem = "";
+
   constructor() { }
 
   ngOnInit() {
 
     $('.has-submenu>a').click(function(){
-      $(this).siblings('.submenu').slideToggle();
-      $(this).parent().toggleClass("active");
-      console.log('TEST');
+      // $(this).siblings('.submenu').slideToggle();
+      // $(this).parent().toggleClass("active");
     });
-    
+
+  }
+
+  test(item: object){
+    // console.log(item.target.attributes['data-test-data'].value);
+    // console.log();
+    $("a[data-type='parent-menu'").siblings('ul.submenu').slideDown();
+  }
+
+  activateMenuItem(item: any){
+
+    // Get name from data-name attribute
+    var name = item.target.attributes['data-name'].value;
+
+    console.log(name);
+
+    // Close all parent menu items
+    $('ul.submenu').slideUp();
+
+    // If closing the menu
+    if(this.activatedMenuItem==name){
+      this.activatedMenuItem="";
+
+    // Opening Menu
+    }else{
+      this.activatedMenuItem=name;
+      $("a[data-type='parent-menu'][data-name="+name+"]").siblings('ul.submenu').slideDown();
+    }
   }
 
 }
